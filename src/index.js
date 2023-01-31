68919416856ea37e7e7b093b83ded28cc15cd734
adhamosama13 <adhamosama13@outlook.com>
2023-01-31 16:35:35 +0200

    sec commit

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
     <App />
-    </HashRouter>
   </React.StrictMode>
 );
 
